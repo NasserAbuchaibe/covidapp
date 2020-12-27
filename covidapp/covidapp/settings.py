b3a8b04d4c3013a0dc6f27d1134062e16fa8adf3
Nasser Abuchaibe <60364142+NasserAbuchaibe@users.noreply.github.com>
2020-12-26 22:57:22 -0500

    Update settings.py
    Changes  
    DEBUG = False
    
    ALLOWED_HOSTS = ['*']

--- covidapp/covidapp/settings.py
+++ covidapp/covidapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e7j@*p_0qjp1e6gyw&#n+_96t@@gai%d%eyf=iso&pv4wjge0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
